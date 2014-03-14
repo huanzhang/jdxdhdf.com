ece97999d4b82e93a30b62bbac077aa8063b4145
Huan Zhang <hzhang@redhat.com>
2014-03-15 00:57:12 +0800

    Layouts of the home page

--- index.php
+++ index.php
@@ -4,4 +4,106 @@
 
 <?php include "include/nav.php"?>
 
+<!--== Carousel ==-->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="/images/slide1.png">
+    </div>
+    <div class="item">
+      <img src="/images/slide2.png">
+    </div>
+    <div class="item">
+      <img src="/images/slide3.png">
+    </div>
+  </div>
+</div>
+<!--== Carousel End ==-->
+
+<!--== Columns ==-->
+<div class="row myPanels">
+  <div class="col-xs-3 leftPanel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+          <h3 class="panel-title">
+            <span class="glyphicon glyphicon-expand"></span>
+            <a href="#">新闻中心</a>
+          </h3>
+      </div>
+      <div class="panel-body">
+          <h6>
+            <small><span class="glyphicon glyphicon-play"></span></small>
+            <a href="#">文物保护传承专用装备产业技术...</a>
+          </h6>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-5 midPanel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          <span class="glyphicon glyphicon-expand"></span>
+          <a href="#">关于我们</a>
+        </h3>
+      </div>
+      <div class="panel-body">
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-4 rightPanel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          <span class="glyphicon glyphicon-expand"></span>
+          <a href="#">经典案例</a>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="row">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--== Columns End ==-->
+
+<!--== Columns ==-->
+<div class="row myPanels">
+  <div class="col-xs-3 leftPanel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+          <h3 class="panel-title">
+            <span class="glyphicon glyphicon-expand"></span>
+            <a href="#">联系我们</a>
+          </h3>
+      </div>
+      <div class="panel-body">
+          <h6>
+            <small><span class="glyphicon glyphicon-play"></span></small>
+            <a href="#">文物保护传承专用装备产业技术...</a>
+          </h6>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-9 midPanel">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          <span class="glyphicon glyphicon-expand"></span>
+          <a href="#">产品展示</a>
+        </h3>
+      </div>
+      <div class="panel-body">
+      </div>
+    </div>
+  </div>
+</div>
+<!--== Columns End ==-->
 <?php include "include/footer.php"?>
