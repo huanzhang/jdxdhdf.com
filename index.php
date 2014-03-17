d9f51e993a753bd867b97f93e5c125a526cf7471
Huan Zhang <hzhang@redhat.com>
2014-03-17 18:43:54 +0800

    Add more content

--- index.php
+++ index.php
@@ -42,7 +42,23 @@
           <div class="panel-body">
               <h6>
                 <small><span class="glyphicon glyphicon-play"></span></small>
-                <a href="#">文物保护传承专用装备产业技术...</a>
+                <a href="#">废旧集装箱的新应用</a>
+              </h6>
+              <h6>
+                <small><span class="glyphicon glyphicon-play"></span></small>
+                <a href="#">集装箱活动房价格指数趋势</a>
+              </h6>
+              <h6>
+                <small><span class="glyphicon glyphicon-play"></span></small>
+                <a href="#">集装箱活动房的优点</a>
+              </h6>
+              <h6>
+                <small><span class="glyphicon glyphicon-play"></span></small>
+                <a href="#">活动房产业发展呈多元化趋势</a>
+              </h6>
+              <h6>
+                <small><span class="glyphicon glyphicon-play"></span></small>
+                <a href="#">住人集装箱与活动房安全性比较</a>
               </h6>
           </div>
         </div>
@@ -58,10 +74,11 @@
               </h3>
           </div>
           <div class="panel-body">
-              <h6>
-                <small><span class="glyphicon glyphicon-play"></span></small>
-                <a href="#">文物保护传承专用装备产业技术...</a>
-              </h6>
+              <h6>联系人：张先生</h6>
+              <h6>电 话：010-61419788</h6>
+              <h6>传 真：010-61419788</h6>
+              <h6>手 机：13366576929</h6>
+              <h6>地 址：北京市顺义区杨镇下坡村，顺平路与木燕路交汇口，停车场东侧</h6>
           </div>
         </div>
       </div>
@@ -78,6 +95,8 @@
             </h3>
           </div>
           <div class="panel-body">
+            <p>北京京东兴达集装箱活动房坐落于顺义区顺平路与木燕路交汇口，是京东地区最专业生产制造集装箱及活动房的。
+京东兴达集装箱活动房专业从事箱式活动房的设计、生产、销售以及租赁一条龙服务，致力于为客户提供一个专业、安全、性价比高的购买及租赁平台。</p>
           </div>
         </div>
       </div>
